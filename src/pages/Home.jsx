88e649916413bea7edae25364ecba863dd6feade
caiosergg <caiosergio123@gmail.com>
2025-02-23 20:30:16 -0300

    feat: projeto finalizado com integração TMDB

---
+++ src/pages/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MoviesGrid.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    console.log(topRatedUrl);
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  console.log(topMovies);
+
+  return (
+    <div className="container">
+      <h2
+        style={{
+          fontFamily: "Impact, Arial Black, sans-serif",
+          color: "#ffcc00",
+          fontSize: "32px",
+          fontWeight: "bold",
+          textAlign: "center",
+          textTransform: "uppercase",
+          letterSpacing: "3px",
+          textShadow: "3px 3px 10px rgba(255, 204, 0, 0.6)",
+          marginBottom: "20px",
+        }}
+      >
+        ⭐ Top 20 Melhores Filmes ⭐
+      </h2>
+
+      <div className="movies-container">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
